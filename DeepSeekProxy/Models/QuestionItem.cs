8873052c9c47ef397d65561f019aa1b375b17048
care4u <2669624618@qq.com>
2025-11-09 19:25:46 +0800

    remove DeeeSeekProxy Floder_.net8

--- DeepSeekProxy/Models/QuestionItem.cs
+++
@@ -1,7 +0,0 @@
-public class QuestionItem
-{
-    public int Id { get; set; }
-    public string Question { get; set; }
-    public string Answer { get; set; }
-    public DateTime CreateTime { get; set; }
-}
