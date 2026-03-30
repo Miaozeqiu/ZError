852cf286afcdd8d150448949fc2cce20fb1e4e05
care4u <2669624618@qq.com>
2026-03-30 17:46:01 +0800

    deleted a lot things

--- src-tauri/test_jieba.rs
+++
@@ -1,1 +0,0 @@
-﻿use jieba_rs::Jieba; fn main() { let jieba = Jieba::new(); let words = jieba.cut_for_search("基本素质", true); println!("{:?}", words); let words2 = jieba.cut("基本素质", true); println!("{:?}", words2); }
